79a21ed8e44edf40e005deabd2648a4f72a098c1
Aarav Patel <aaravpatel14.in@gmail.com>
2026-03-22 14:23:11 +0530

    config.py

--- config.py
+++ config.py
@@ -6,15 +6,15 @@ class Config:
     LICHESS_TOKEN: str = os.getenv("LICHESS_TOKEN", "YOUR_TOKEN_HERE")
     STOCKFISH_PATH: str = r"C:\Users\Aarav\OneDrive\Documents\Codes\pythonCodes\Chess\Stockfish\stockfish\stockfish-windows-x86-64-avx2.exe"
     BOOK_PATH: str = "gm2001.bin"
-
+    
     CPL_DEPTH_MAP: list[tuple[int, int]] = [
-        (15,  14),   # was 22
-        (25,  12),   # was 20
-        (40,  10),   # was 17
-        (60,   8),   # was 14
-        (90,   6),   # was 11
-        (130,  4),   # was 8
-        (999,  2),   # was 5
+            (15,   8),
+            (25,   7),
+            (40,   6),
+            (60,   5),
+            (90,   4),
+            (130,  3),
+            (999,  2),
     ]
 
     CPL_MIN_SAMPLES: int = 3
